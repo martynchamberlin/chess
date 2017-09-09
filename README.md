0ac9416352f8227f127b3e065e2939deb5526a20
Martyn Chamberlin <martynchamberlin@icloud.com>
2017-09-09 17:30:16 -0500

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# chess
-An open-source implementation of the public Chess.com API 
+# Chess
+An open-source implementation of the [public Chess.com API](https://www.chess.com/clubs/forum/chess-com-developer-community). 
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const webpack = require('webpack');
+
+module.exports = {
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    disableHostCheck: true,
+    publicPath: '/',
+    inline: false,
+    hot: true,
+    host: 'localhost',
+    clientLogLevel: 'info',
+    contentBase: './',
+    watchOptions: {
+      ignored: /node_modules/
+    }
+  },
+  devtool: 'cheap-module-source-map',
+  entry: {
+    main: [
+      './src/index.js',
+      'webpack-dev-server/client?/sockjs-node/',
+      'webpack/hot/dev-server'
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: 'vue-loader',
+        options: {
+          cssModules: {
+            localIdentName: '[name]-[local]'
+          },
+          loaders: {
+            js: 'babel-loader',
+            css: ['vue-style-loader', 'css-loader'],
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader/url',
+            options: {
+              insertAt: 'top'
+            }
+          },
+          { loader: 'file-loader' }
+        ],
+      },
+      {
+        test: /\.(jpe?g|gif|png|woff|woff2|ttf|eot|svg)$/,
+        loader: 'file-loader?emitFile=false&name=[path][name].[ext]'
+      }
+    ]
+  },
+  output: {
+    path: '/',
+    filename: 'dist/[name].js',
+    publicPath: `http://localhost:8080/`
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
